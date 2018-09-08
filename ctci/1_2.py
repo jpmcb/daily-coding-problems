4ac56250a3f17a91ebbb520daf4be6b5d7e36b3d
John McBride <jpmmcbride@gmail.com>
2018-09-08 11:38:44 -0600

    Wip

---
+++ ctci/1_2.py
@@ -0,0 +1,15 @@
+# Check permutation:
+# Given two strings, write a method to decide if one is a perm of the other
+
+import unittest
+
+def isPermutation(x, y):
+
+class Test(unittest.TestCase):
+    dataT = []
+    dataF = []
+
+    def test_isPermutation(self):
+
+if(__name__ == '__main__'):
+    unittest.main()
