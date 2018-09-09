7794211462d8b71414b906d34fcb9174956719ca
John McBride <jpmmcbride@gmail.com>
2018-09-09 12:44:48 -0600

    Finished strings 2 problem

--- ctci/1_2.py
+++ ctci/1_2.py
@@ -4,12 +4,45 @@
 import unittest
 
 def isPermutation(x, y):
+    if(len(x) != len(y)):
+        return False
+
+    table = {}
+    for char in x:
+        table[char] = 0
+
+    for char in x:
+        table[char] += 1
+
+    for char in y: 
+        if char not in table:
+            return False
+        if table[char] == 0:
+            return False
+        table[char] -= 1
+
+    return True 
 
 class Test(unittest.TestCase):
-    dataT = []
-    dataF = []
+    dataT = [
+        ['abcd', 'dcba'],
+        ['345543', '543345']
+    ]
+    dataF = [
+        ['abcd', 'abce'],
+        ['abcde', 'abc'],
+        ['willywonka', 'willykonka']
+    ]
 
     def test_isPermutation(self):
+        # True checks
+        for data in self.dataT:
+            result = isPermutation(*data)
+            self.assertTrue(result)
+
+        for data in self.dataF:
+            result = isPermutation(*data)
+            self.assertFalse(result)
 
 if(__name__ == '__main__'):
     unittest.main()
