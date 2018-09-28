9dbb83598b8c61e6d9c5adac370b4d33e35b299a
John McBride <jpmmcbride@gmail.com>
2018-09-28 16:30:33 -0600

    Working progress stack of stacks

---
+++ ctci/3_3.py
@@ -0,0 +1,19 @@
+# Stack list
+
+class Stack():
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.list = []
+
+    def pop(self):
+        pass
+
+    def push(self):
+        pass
+
+    def is_full(self):
+        pass
+
+    def is_empty(self):
+        pass
